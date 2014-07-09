f8ae8b1d250d0476244b98308d11faf51ecb0cda
Jacob Howard <jacob@havoc.io>
2014-07-09 15:02:11 +0100

    Added a common version check module.

---
+++ modules/version_check.py
@@ -0,0 +1,20 @@
+# System imports
+from sys import version_info, exit
+
+
+def owls_python_version_check():
+    """Checks that the current version of Python is supported by OWLS packages,
+    exiting if not.
+    """
+    # Get the Python version, up to the minor revision
+    python_version = version_info[0:2]
+
+    # Don't let OWLS modules install an unsupported python version
+    supported_python_versions = (
+        (2, 7),
+        (3, 2),
+        (3, 3),
+        (3, 4),
+    )
+    if python_version not in supported_python_versions:
+        exit('unsupported Python version')
